9df198f6fa20765a5d7396b8195be4850c2dc2a2
Chermash Igor <chermash.igor@gmail.com>
2025-08-03 09:35:48 +0300

    Added 404 page

---
+++ error.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <VideoBackground/>
+    <div class="error-block">
+      <img src="/images/logo-text.svg" alt="" class="mb-32" />
+      <h2>
+        {{ error.statusCode === 404 ? 'Page Not Found' : 'An error occurred' }}
+      </h2>
+      <NuxtLink to="/">
+        <ArrowButton class="mt-32"
+          color="gold"
+          arrowRotate="-90"
+        />
+      </NuxtLink>
+    </div>
+  </section>
+  
+</template>
+
+<script setup>
+import VideoBackground from '~/components/VideoBackground.vue';
+defineProps({
+  error: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+section{
+  overflow: hidden;
+  position: relative;
+  height: 100dvh;
+  z-index: 2;
+}
+.error-block{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  color: rgb(var(--color-white));
+  transition: 500ms;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
