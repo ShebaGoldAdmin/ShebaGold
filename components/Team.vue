0cf7e3dce6d458fca6b6d1b3ded8d9f04fbe8d75
Chermash Igor <chermash.igor@gmail.com>
2025-08-07 09:39:09 +0300

    MOD-123 Team Section Updates

--- components/Team.vue
+++ components/Team.vue
@@ -1,45 +1,27 @@
 <template>
   <section class="pt-80 pb-80 pt-184-sm pb-144-sm position-relative">
     <div class="container container--sm">
+      <div class="team-sliding-heading" ref="slidingHeadingRef">Team</div>
       <h2 class="word-split mb-24 mb-72-sm">Team</h2>
-      <div class="team-sliding-heading" ref="slidingHeadingRef">Eli B.Abisror</div>
       <div class="row">
-        <div class="col-3">
+        <div class="col-4">
           <img class="team-logo" src="public/images/logo-rounded.svg" alt="Sheba Gold Capital" /> 
         </div>
-        <div class="col-sm-9">
-          <h5 class="word-split">Extensive Experience and Success in Investment and Healthcare Solutions
+        <div class="col-sm-8">
+          <h5 class="word-split mb-32">Extensive Experience and Success in Investment and Healthcare Solutions
           </h5>
+          <p class="lg scalable">We believe strong companies start with fulfilled teams and a shared sense of purpose. When people thrive, success follows.<br>
+            The Sheba Gold Capital team is made up of seasoned professionals with deep expertise across the investment and healthcare landscapes. Each member of the team brings excellence,  passion and pride to everything they do.
+          </p>
         </div>
       </div>
-      <div class="row justify-content-end">
-        <div class="col-md-9">
-          <div class="row mt-80 mt-184-sm">
-            <div class="col-sm-5 pr-32-md">
-              <div class="team-img">
-                <TeamImage src="/images/team/eli-abisror.png"/>
-              </div>
-            </div>
-            <div class="col-sm-7 pl-40-lg d-flex flex-column align-items-start">
-              <p class="scalable">Based in Tinton Falls, he has expertly overseen the distribution of medical and dental supply products, leveraging his extensive knowledge in the medical export and import sectors. </p>
-              <p class="scalable mb-40">He is currently CEO of 'Mission Medical' - founded in 2006, and has since concurrently launched multiple international healthcare supply companies - spanning 30 countries across six continents.</p>
-              <NuxtLink to="/team/eli-b-abisror">
-                <ArrowButton class="mt-auto from-right"
-                  arrowRotate="-90"
-                >
-                  Find out more
-                </ArrowButton>
-              </NuxtLink>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TeamCarousel class="mt-96"/>
     </div>
   </section>
 </template>
 
 <script setup>
-import TeamImage from './elements/TeamImage.vue';
+import TeamCarousel from './TeamCarousel.vue';
 import { ref, onMounted} from 'vue';
 
 const { $gsap } = useNuxtApp();
@@ -47,39 +29,9 @@ const { $gsap } = useNuxtApp();
 const slidingHeadingRef = ref(null);
 
 onMounted(() => {
-  const teamImgWrap = document.querySelector('.team-img');
-  const teamImg = teamImgWrap.querySelector('img') ;
-  const teamLogo = teamImgWrap.querySelector('svg') ;
-
-  let tl = $gsap.timeline({
-    scrollTrigger: {
-      trigger: teamImgWrap,
-      start: 'top bottom',
-      toggleActions: "restart none none reset"
-    }
-  });
-
-  tl.set(teamImgWrap, { autoAlpha: 1 });
-  tl.fromTo(
-    teamImgWrap, 3, 
-    { xPercent: -100 },
-    { xPercent: 0, delay: -3, ease: 'power4.out' }
-  );
-  tl.from(teamImg, 3, {
-    xPercent: 100,
-    scale: 1.3,
-    delay: -3,
-    ease: 'power4.out',
-  });
-  tl.from(teamLogo, 3, {
-    xPercent: 100,
-    delay: -3,
-    ease: 'power4.out',
-  });
-
   $gsap.fromTo(
     slidingHeadingRef.value,
-    { x: -300 },
+    { x: -100 },
     {
       x: 600,
       scrollTrigger: {
@@ -95,34 +47,27 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 .team-sliding-heading{
-  @include fsz(250px);
-  letter-spacing: 12.5px;
+  @include fsz(348px);
+  letter-spacing: 5%;
   font-family: var(--font-heading);
   color: rgb(var(--color-light-gray));
   position: absolute;
-  top: 490px;
+  bottom: 0;
   white-space: nowrap;
   @include respond-to(sm){
     @include fsz(130px);
     letter-spacing: 6.5px;
-    top: 600px;
-  }
-}
-.team-img{
-  overflow: hidden;
-  @include respond-to(sm){
-    margin-left: 25%;
-    margin-bottom: 122px;
   }
 }
 .team-logo{
   margin: -24px 0 0 16px;
+  width: 148px;
   @include respond-to(sm){
     margin: 0;
     position: absolute;
     z-index: 2;
-    top: 144px;
-    left: 3%;
+    top: -100px;
+    right: 0;
     width: 120px;
   }
 }
--- components/TeamCarousel.vue
+++ components/TeamCarousel.vue
@@ -1,92 +1,76 @@
 <template>
-  <main>
-    <InnerTop heading="Team"/>
-    <section class="pt-112 pt-152-sm pb-80 pb-192-sm">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8">
-            <HelperText class="mb-24">Meet Our Team</HelperText>
-            <h2 class="mb-64 mb-80-sm word-split">With 20 years of experience in the business and real estate worlds.</h2>
-            <div class="team-section">
-              <div class="row scalable">
-                <div class="col-sm-6 pr-120-md d-none d-block-sm">
-                  <transition name="transition-slide-y" mode="out-in">
-                    <div v-if="activeMember && activeMember.description.length > 0" 
-                      :key="activeMember"
-                    >
-                      <p class="mt-24">{{ activeMember.description[0] }}</p>
-                    </div>
-                  </transition>
-                </div>
-                <div class="col-10 col-sm-6">
-                  <div 
-                    class="team-carousel"
-                    @touchstart="onTouchStart"
-                    @touchmove="onTouchMove"
-                    @touchend="onTouchEnd"
-                  >
-                    <div 
-                      class="team-carousel__item"
-                      v-for="(member, index) in teamMembers" 
-                      :key="index"
-                      @click="handleCardClick(index)"
-                    >
-                      <TeamImage :src="member.image" />
-                      <div class="team-carousel__overlay"></div>
-                      <div v-show="index === activeMemberIndex" class="team-carousel__view-more">
-                        <span>View More</span>
-                      </div>
-                    </div>
-                  </div>
-                  <transition name="transition-slide-y" mode="out-in">
-                    <div v-if="activeMember && activeMember.description.length > 0"
-                      class="team-carousel__item-data"
-                      :key="activeMember"
-                    >
-                      <h3>{{ activeMember.name }}</h3>
-                      <p>{{ activeMember.position }}</p>
-                    </div>
-                  </transition>
+  <div class="row">
+    <div class="col-lg-8">
+      <div class="team-section">
+        <div class="row scalable">
+          <div class="col-sm-6 pr-120-md d-none d-block-sm">
+            <transition name="transition-slide-y" mode="out-in">
+              <div v-if="activeMember && activeMember.description.length > 0" 
+                :key="activeMember"
+              >
+                <p class="mt-24">{{ activeMember.description[0] }}</p>
+              </div>
+            </transition>
+          </div>
+          <div class="col-10 col-sm-6">
+            <div 
+              class="team-carousel"
+              @touchstart="onTouchStart"
+              @touchmove="onTouchMove"
+              @touchend="onTouchEnd"
+            >
+              <div 
+                class="team-carousel__item"
+                v-for="(member, index) in teamMembers" 
+                :key="index"
+                @click="handleCardClick(index)"
+              >
+                <TeamImage :src="member.image" />
+                <div class="team-carousel__overlay"></div>
+                <div v-show="index === activeMemberIndex" class="team-carousel__view-more">
+                  <span>View More</span>
                 </div>
               </div>
-              <div class="row justify-content-end">
-                <div class="col-12 col-sm-6">
-                  <div class="d-flex align-items-center justify-content-center mt-40">
-                    <ArrowButton class="ml-8 mr-8"
-                      @click="prevMember" 
-                      arrowRotate="90"
-                    />
-                    <ArrowButton class="ml-8 mr-8"
-                      @click="nextMember"
-                      arrowRotate="-90"
-                    />
-                  </div>
-                </div>
+            </div>
+            <transition name="transition-slide-y" mode="out-in">
+              <div v-if="activeMember && activeMember.description.length > 0"
+                class="team-carousel__item-data"
+                :key="activeMember"
+              >
+                <h3>{{ activeMember.name }}</h3>
+                <p>{{ activeMember.position }}</p>
               </div>
+            </transition>
+          </div>
+        </div>
+        <div class="row justify-content-end">
+          <div class="col-12 col-sm-6">
+            <div class="d-flex align-items-center justify-content-center mt-40">
+              <ArrowButton class="ml-8 mr-8"
+                @click="prevMember" 
+                arrowRotate="90"
+              />
+              <ArrowButton class="ml-8 mr-8"
+                @click="nextMember"
+                arrowRotate="-90"
+              />
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </main>
+    </div>
+  </div>
 </template>
-
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import TeamImage from './elements/TeamImage.vue';
 import teamData from '~/data/team.json';
 
-import InnerTop from '~/components/InnerTop.vue';
-import TeamImage from '~/components/elements/TeamImage.vue';
+const { $gsap } = useNuxtApp();
+const router = useRouter();
 
 const teamMembers = ref(teamData);
 const activeMemberIndex = ref(0);
 
-useHead({
-  title: 'Team',
-});
-
-const { $gsap } = useNuxtApp();
-const router = useRouter();
 let teamItems = [];
 const touchStartX = ref(0);
 const touchEndX = ref(0);
@@ -198,7 +182,6 @@ onMounted(() => {
   }
 });
 </script>
-
 <style lang="scss" scoped>
 .team-carousel {
   position: relative;
